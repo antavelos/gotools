71e065cacafa23d4ee321bfa11d553db47e9c4ed
Alexandros Ntavelos <a.ntavelos@gmail.com>
2017-07-12 22:20:44 +0200

    add spiral tests

---
+++ spiral/spiral_test.go
@@ -0,0 +1,30 @@
+package spiral
+
+import (
+	"bytes"
+	"github.com/antavelos/gotools/geometry"
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	for _, c := range []struct {
+		p    geometry.Point
+		n    int
+		step float64
+		want string
+	}{
+		{geometry.Point{}, 5, 1.0, "(0, 0)(1, 0)(1, 1)(0, 1)(-1, 1)"},
+		{geometry.Point{}, 10, 0.1, "(0, 0)(0.1, 0)(0.1, 0.1)(0, 0.1)(-0.1, 0.1)(-0.1, 0)(-0.1, -0.1)(0, -0.1)(0.1, -0.1)(0.2, -0.1)"},
+		{geometry.Point{5, 5}, 5, 0.1, "(5, 5)(5.1, 5)(5.1, 5.1)(5, 5.1)(4.9, 5.1)"},
+	} {
+
+		var buf bytes.Buffer
+		for p := range Spiral(c.p, c.n, c.step) {
+			buf.WriteString(p.String())
+		}
+		got := buf.String()
+		if got != c.want {
+			t.Errorf("Spiral(%v, %v, %v) == %q, want %q", c.p, c.n, c.step, got, c.want)
+		}
+	}
+}
